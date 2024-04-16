1f28abe71780afb013075b427713bb25bf8dbd17
Mayur Gulhane <gulhanemayur412@gmail.com>
2024-04-16 23:09:19 +0530

    django channels basic setup

---
+++ django_channel/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for django_channel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from apk.consumers import MyConsumer
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channel.settings')
+
+application = get_asgi_application()
+
+ws_patterns = [
+    path('ws/test/', MyConsumer.as_asgi()),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter(ws_patterns)
+})
